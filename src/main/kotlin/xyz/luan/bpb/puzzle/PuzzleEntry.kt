b67d77a2be6e77a22f8e6c2d80e50b61c6709bd9
Luan Nico <luanpotter27@gmail.com>
2026-04-01 00:07:47 -0400

    feat: Add actual puzzles

--- src/main/kotlin/xyz/luan/bpb/puzzle/PuzzleEntry.kt
+++ src/main/kotlin/xyz/luan/bpb/puzzle/PuzzleEntry.kt
@@ -8,8 +8,14 @@ internal data class PuzzleEntry(
     val PUZZLES =
         listOf(
             PuzzleEntry("Kept behind locked doors.", "XXxxxX"),
-            PuzzleEntry("Clue 2", "XxxxXXX"),
-            PuzzleEntry("Clue 3", "xxxXXX"),
+            PuzzleEntry(
+                "Can affect one greatly when made by someone with a strong spirit.",
+                "XxxxXXX",
+            ),
+            PuzzleEntry("Makes the validity of a statement clear.", "xxxXXX"),
+            PuzzleEntry("On a certain scale, this is very hot", "XXXxxx"),
+            PuzzleEntry("Informs you that there's no school today.", "XXXxxx"),
+            PuzzleEntry("A group composed of members that have similar characteristics.", "XXxxxx"),
         )
   }
 }
